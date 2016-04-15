0ecf34de55f604320c9a28c9b7a1693f9d341010
Derek Graham <derekgraham@mac.com>
2016-04-15 13:48:01 -0700

    problem 3 sumAndMultiply

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // This is js file for lab-april-15
 // DSG  added function for problem1
+// DSG added function for problem2
+// DSG added function for problem 3
 function sum(numOne, numTwo) {
   console.log('"The sum of ' + numOne + ' and ' + numTwo + ' is ' + (numOne + numTwo) + '."');
 }
 function multiply(numOne, numTwo) {
   console.log('"The product of ' + numOne + ' and ' + numTwo + ' is ' + (numOne * numTwo) + '."');
+}
+function sumAndMultiply( numOne, numTwo, numThree) {
+  console.log('"' + numOne + ' and ' + numTwo + ' and ' + numThree + ' sum to ' + (sum(sum(numOne,numTwo),numThree)) + '."');
+  console.log('"The numbers ' + numOne + ' and ' + numTwo + ' and ' + numThree + ' have a product of ' + (multiply(multiply(numOne,numTwo),numThree)) + '."');
 }
