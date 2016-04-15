65ad45a905c34fd392bc64739ec899f50490b9a9
Derek Graham <derekgraham@mac.com>
2016-04-15 16:32:28 -0700

    updated HTML and function to update HTML with question text

--- index.js
+++ index.js
@@ -12,13 +12,41 @@ function multiply(numOne, numTwo) {
 }
 
 function sumAndMultiply( numOne, numTwo, numThree) {
-  console.log('"' + numOne + ' and ' + numTwo + ' and ' + numThree + ' sum to ' + (sum(sum(numOne,numTwo),numThree)) + '."');
-  console.log('"The numbers ' + numOne + ' and ' + numTwo + ' and ' + numThree + ' have a product of ' + (multiply(multiply(numOne,numTwo),numThree)) + '."');
+  var result = [sum(sum(numOne,numTwo),numThree), multiply(multiply(numOne,numTwo),numThree)];
+  console.log('"' + numOne + ' and ' + numTwo + ' and ' + numThree + ' sum to ' + result[0] + '."');
+  console.log('"The numbers ' + numOne + ' and ' + numTwo + ' and ' + numThree + ' have a product of ' + result[1] + '."');
+  return result;
 }
 
 function sumArray(myArray){
-  console.table(myArray);
+  console.table([myArray]);
   console.log(myArray + ' was passed in as an array of numbers, and ' +
     ( function () { var mySum = 0; for (var i = 0; i < myArray.length ; i++) { mySum += myArray[i]; } return mySum; } () )
   + ' is their sum.');
+}
+
+function multiplyArray(myArray){
+  console.table([myArray]);
+  console.log('The numbers ' + myArray + ' have a product of ' +
+  ( function () { var myResult = 1; for (var i = 0; i < myArray.length ; i++) { myResult *= myArray[i]; } return myResult; } () )
+  + '.'
+);
+}
+
+function writeQuestionsToHTML(){
+
+  var myResult = document.getElementById('p1');
+  myResult.innerHTML = '<em>(Don\'t forget to create a new branch!)</em> Write a function called <b>sum()</b> that takes in two numbers as arguments and then returns the sum of those numbers.';
+
+  myResult = document.getElementById('p2');
+  myResult.innerHTML = 'Write a function called <b>multiply()</b> that takes in two numbers as arguments and then returns the product of those numbers. ';
+
+  myResult = document.getElementById('p3');
+  myResult.innerHTML = 'Write a function called <b>sumAndMultiply()</b> that takes in THREE numbers as arguments and then returns two values in an array: the sum of those numbers and the product of those numbers.';
+
+  myResult = document.getElementById('p4');
+  myResult.innerHTML = 'Write a function called <b>sumArray()</b> that takes in an array of numbers as a single argument and then returns the sum of those numbers.';
+
+  myResult = document.getElementById('p5');
+  myResult.innerHTML = 'Write a function called <b>multiplyArray()</b> that takes in an array of numbers as a single argument and then returns the product of those numbers.';
 }
