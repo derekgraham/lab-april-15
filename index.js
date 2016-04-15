e3cbf504c39d20a980bbf8fb4a6ecbfeab5d4f42
Derek Graham <derekgraham@mac.com>
2016-04-15 13:33:35 -0700

    Problem 1 sum function

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // This is js file for lab-april-15
+// DSG  added function for problem1
+function sum(numOne, numTwo) {
+  console.log('"The sum of ' + numOne + ' and ' + numTwo + ' is ' + (numOne + numTwo) + '."');
+}
